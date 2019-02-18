fc39761c1a538c8e70a55734d750a948e562858c
James Kominick <james@kominick.com>
2019-02-18 09:27:36 -0500

    add cli commands

--- src/stash/core.clj
+++ src/stash/core.clj
@@ -1,5 +1,6 @@
 (ns stash.core
   (:require [taoensso.timbre :as t]
+            [clojure.tools.cli :refer [parse-opts]]
             [manifold.deferred :as d]
             [aleph.http :as http]
             [byte-streams :as bs]
@@ -7,6 +8,7 @@
             [ring.middleware.params :as params]
             [ring.util.request :as ring-request]
             [compojure.response :refer [Renderable]]
+            [clojure.java.jdbc :as j]
             [stash.router :as router]
             [stash.execution :as ex]
             [stash.database :as db])
@@ -125,8 +127,73 @@
   (use -ns :reload))
 
 
+(defn add-user [name-]
+  (j/with-db-transaction [conn (db/conn)]
+    (let [user (j/insert! conn :app_users {:name name-} {:result-set-fn first})
+          user-id (:id user)
+          _ (prn user-id)
+          uuid (u/uuid)
+          auth-token (j/insert! conn :auth_tokens {:app_user user-id
+                                                   :token uuid})
+          token-str (u/format-uuid uuid)]
+      (println
+        (format "Created user (%s) with auth token %s" user-id token-str)))))
+
+
+(def cli
+  [["-p" "--port PORT" "Port to listen on"
+    :default 3003
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 65536) "Must be 0..=65536"]]
+   ["-n" "--name NAME" "Name to use when creating new user"
+    :validate [#(not (empty? %)) "Name is required"]]
+   ["-h" "--help"]])
+
+
+(defn usage [opts]
+  (->> ["Stash"
+        ""
+        "Usage: stash [options] command"
+        ""
+        "Options:"
+        opts
+        ""
+        "Commands:"
+        "  serve     Start server listening on PORT"
+        "  add-user  Create a new user with NAME"]
+       (clojure.string/join \newline)))
+
+
+(defn has-required [command opts]
+  (cond
+    (and
+      (= command "serve")
+      (some? (:port opts)))   true
+    (and
+      (= command "add-user")
+      (some? (:name opts)))   true
+    :else                     false))
+
+
+(defn parse-args [args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli)]
+    (cond
+      (:help options)     {:msg (usage summary) :ok? true}
+      errors              {:msg (str "Error:\n" (clojure.string/join \newline errors))}
+      (and (= 1 (count arguments))
+           (#{"serve" "add-user"} (first arguments))
+           (has-required (first arguments) options))
+                          {:command (first arguments) :opts options}
+      :else               {:msg (usage summary)})))
+
+
 (defn -main
-  [& _]
-  (let [port (-> (System/getProperty "STASH_PORT" "3003")
-                 (Integer.))]
-    (start port)))
+  [& args]
+  (let [{:keys [command opts msg ok?]} (parse-args args)]
+    (if msg
+      (do
+        (println msg)
+        (System/exit (if ok? 0 1)))
+      (case command
+         "serve" (start (:port opts))
+         "add-user" (add-user (:name opts))))))
