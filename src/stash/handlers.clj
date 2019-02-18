6f57208dbe8266e3e47299abf0744184271378db
James Kominick <james@kominick.com>
2019-02-18 09:30:22 -0500

    require auth for create/retrieve

--- src/stash/handlers.clj
+++ src/stash/handlers.clj
@@ -64,12 +64,37 @@
 
 
 (defn stream-to-file [src file]
-  (d/future
+  (d/future-with
+    ex/fs
     (with-open [file-stream (io/output-stream file)]
       (io/copy src file-stream))))
 
 
-(defn new-digest [] (MessageDigest/getInstance "sha-256"))
+(defn ex-invalid-request
+  [& {:keys [e-msg resp-msg] :or {e-msg "Missing request access token"
+                                  reps-msg "expected access token header"}}]
+  (ex-info e-msg
+           {:type :invalid-request
+            :msg e-msg
+            :resp (->resp :status 400 :body e-msg)}))
+
+(defn ex-not-found
+  [& {:keys [e-msg resp-msg] :or {e-msg "thing not found"
+                                  resp-msg "item not found"}}]
+  (ex-info e-msg
+           {:type :invalid-request
+            :msg e-msg
+            :resp (->resp :status 404 :body resp-msg)}))
+
+
+(defn get-request-user-token [req]
+  (->
+    (:headers req)
+    (get "x-stash-access-token")
+    ((fn [token]
+       (if (nil? token)
+         (throw (ex-invalid-request))
+         token)))))
 
 
 (defn new-upload-path [token]
@@ -87,6 +112,7 @@
         upload-file (io/file upload-path)
         size (atom 0)
         supplied-token (-> req :params :supplied-token)
+        user-auth-token (-> (get-request-user-token req) u/parse-uuid)
         data-stream (s/stream buf-size)
         connect-fn (fn [^PooledSlicedByteBuf buf]
                      (do
@@ -98,20 +124,28 @@
       (d/future-with
         ex/cpu
         (j/with-db-transaction [conn (db/conn)]
-          (j/insert! conn :items {:path upload-path
-                                  :stash_token token
-                                  :supplied_token supplied-token
-                                  :size @size})))
+          (let [auth (j/query conn
+                              ["select app_user from auth_tokens where token = ?" user-auth-token]
+                              {:result-set-fn first})]
+            (when auth
+              (j/insert! conn :items {:path upload-path
+                                      :stash_token token
+                                      :supplied_token supplied-token
+                                      :creator (:app_user auth)
+                                      :size @size}
+                         {:result-set-fn first})))))
       (fn [item]
-        (do
-          (t/info "uploading item" item "to" upload-path)
-          item))
+        (if (nil? item)
+          (throw (Exception. "Error creating item"))
+          (do
+            (t/info "uploading item" (:id item) "to" upload-path)
+            item)))
       (fn [item]
         (stream-to-file body upload-file)
         item)
       (fn [item] (->json {:ok :ok
                           :size @size
-                          :token token-str})))))
+                          :stash_token (-> (:stash_token item) u/format-uuid)})))))
 
 
 (defn stream-from-file [sink file-name]
@@ -140,35 +174,58 @@
           (format "Backing file (%s) for item (%s) does not exist" file-path item-id))
         false))))
 
+(defn get-item-by-tokens [stash-token supplied-token request-user-token]
+  (j/with-db-transaction [conn (db/conn)]
+    (let [item (j/query conn
+             [(str
+                "select items.* from items"
+                " inner join auth_tokens auth on auth.app_user = items.creator"
+                " where items.stash_token = ?"
+                "   and items.supplied_token = ?"
+                "   and auth.token = ?")
+              stash-token
+              supplied-token
+              request-user-token]
+             {:result-set-fn first})
+          ]
+      (t/infof "found item %s" (:id item))
+      item)))
 
 (defn retrieve [req]
   (let [supplied-token (-> req :params :supplied-token)
+        request-user-token (-> (get-request-user-token req) u/parse-uuid)
         stash-token (-> req
                         :body
                         (bs/to-string)
                         (s->map)
                         (get "stash_token")
                         (u/parse-uuid))
-        _ (prn supplied-token stash-token)
         sink (s/stream buf-size)
         resp (s/stream buf-size)
         _ (s/connect sink resp {:description "file transfer stream"})]
-    (d/chain
-      (d/future-with
-        ex/cpu
-        (j/with-db-transaction [conn (db/conn)]
-          (j/query conn ["select * from items where stash_token = ? and supplied_token = ?"
-                         stash-token
-                         supplied-token]
-                   {:result-set-fn first})))
-      (fn [item]
-        (if (or
+    (if (nil? request-user-token)
+      (->resp :status 401 :body "unauthorized")
+      (d/chain
+        (d/future-with
+          ex/cpu
+          (get-item-by-tokens stash-token supplied-token request-user-token))
+        (fn [item]
+          (cond
+            (or
               (nil? item)
               (not (backing-file-exists item)))
-          (->resp :status 404 :body "nothing to see here")
-          (d/chain
-            (stream-from-file sink (:path item))
-            (fn [_]
-              (->resp :headers {"content-type" "text/plain"}
-                      :body resp))))))))
+                  (->resp :status 404 :body "nothing to see here")
+            (and
+              (some? (:creator_token item))
+              (not (= (:creator_token item) request-user-token)))
+                  (do
+                    (t/info "invalid access token")
+                    (->resp :status 401 :body "unauthorized"))
+            :else
+                  (d/chain
+                    (stream-from-file sink (:path item))
+                    (fn [_]
+                      (->resp
+                        :headers {"content-type" "text/plain"}
+                        :body resp)))))))))
 
