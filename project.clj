926a5b79e4452f9395a51ef0db2ad11ca09219c0
James Kominick <james@kominick.com>
2019-02-18 23:17:55 -0500

    update project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject stash "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject stash "0.1.0"
+  :description "Simple file storage system"
+  :url "https://github.com/jaemk/stash"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
