0c209a900854127fde3d45510537f9ab5a4262a8
James Kominick <james@kominick.com>
2019-05-04 16:03:50 -0400

    update test layout
    - move setup/teardown to a test-utils file for reuse

--- test/stash/core_test.clj
+++ test/stash/core_test.clj
@@ -1,91 +1,82 @@
 (ns stash.core-test
   (:use midje.sweet)
   (:require [stash.database.core :as db]
-            [hikari-cp.core :refer [make-datasource
-                                    close-datasource]]
-            [clojure.java.jdbc :as jdbc]
             [stash.utils :as u]
-            [migratus.core :as migratus]
-            [taoensso.timbre :as t]))
+            [stash.test-utils :refer [setup-db
+                                      teardown-db
+                                      truncate-db]]))
 
 
-(defonce test-db-name (atom nil))
 (defonce test-db (atom nil))
 
-(defn teardown-db []
-  (swap! test-db
-         (fn [db]
-           (when (not (nil? db))
-             (try
-               (t/info "closing test db")
-               (close-datasource (:datasource db))
-               (catch Exception e
-                 (t/warn "ignoring error" e))))
-           nil))
-  (swap! test-db-name
-         (fn [db-name]
-           (when (not (nil? db-name))
-             (try
-               (t/info "dropping test db")
-               (jdbc/execute! (db/conn)
-                              [(str "drop database " db-name)]
-                              {:transaction? false})
-               (catch Exception e
-                 (t/warn "ignoring error" e))))
-
-           nil)))
-
-(defn setup-db []
-  (teardown-db)
-  (let [db-name (->> (u/uuid)
-                     u/format-uuid
-                     (str "stash_test_"))]
-    (t/info "setting up test db" db-name)
-    (jdbc/execute! (db/conn)
-                   [(str "create database " db-name)]
-                   {:transaction? false})
-    (reset! test-db-name db-name)
-    (let [ds (-> db/db-config
-                 (assoc :database-name db-name)
-                 (assoc :maximum-pool-size 1)
-                 (make-datasource))
-          conn {:datasource ds}
-          config (db/migration-config conn)]
-      (reset! test-db conn)
-      (migratus/migrate config))))
-
-
 (with-state-changes
-  [(before :contents (setup-db))
-   (after :contents (teardown-db))]
+  [(before :contents (setup-db test-db))
+   (after :contents (teardown-db test-db))
+   (before :facts (truncate-db test-db))]
   (facts
     "here we go"
     (fact
       "we can add"
       (+ 1 2) => 3
       (+ 2 3) => 5)
+
     (fact
       "we can do db things"
       (nil? @test-db) => false
-      (db/create-user @test-db "bean") =>
-        (fn [a]
-          (and (= (-> a :user :name) "bean")
-               (some? (-> a :auth :token))))
-      (db/select-users @test-db) =>
-        (fn [a]
-          (and (= 1 (count a))
+      (db/create-user (:conn @test-db) "bean") =>
+        (fn [result]
+          (and (= (-> result :user :name) "bean")
+               (some? (-> result :auth :token))))
+      (db/select-users (:conn @test-db)) =>
+        (fn [result]
+          (and (= 1 (count result))
                (= "bean"
-                  (-> (first a) :name))))
+                  (-> (first result) :name))))
       (db/create-item
-        @test-db
+        (:conn @test-db)
         {:token (u/uuid)
          :name "thing"
          :creator_id (-> (db/select-users
-                           @test-db
+                           (:conn @test-db)
                            :where [:= :name "bean"])
                          first
                          :id)}) =>
-        (fn [a]
-          (t/warn a)
-          (= "thing" (:name a))))))
+        (fn [result]
+          (= "thing" (:name result))))
 
+    (fact
+      "we truncated the db"
+      (db/select-users (:conn @test-db)) =>
+        (fn [result]
+          (empty? result)))))
+
+
+(defonce test-db2 (atom nil))
+
+(with-state-changes
+  [(before :contents (setup-db test-db2))
+   (after :contents (teardown-db test-db2))
+   (before :facts (truncate-db test-db2))]
+  (facts
+    "we can do db things separately"
+    (nil? @test-db2) => false
+    (db/create-user (:conn @test-db2) "bean") =>
+    (fn [result]
+      (and (= (-> result :user :name) "bean")
+           (some? (-> result :auth :token))))
+    (db/select-users (:conn @test-db2)) =>
+    (fn [result]
+      (and (= 1 (count result))
+           (= "bean"
+              (-> (first result) :name))))
+    (db/create-item
+      (:conn @test-db2)
+      {:token (u/uuid)
+       :name "thing"
+       :creator_id (-> (db/select-users
+                         (:conn @test-db2)
+                         :where [:= :name "bean"])
+                       first
+                       :id)}) =>
+    (fn [result]
+      (= "thing" (:name result)))))
